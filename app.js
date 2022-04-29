baae48d9325ce1fa6da35be591861aeb1096f137
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-04-28 18:02:30 -0700

    required user input for some questions

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
-        },
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+                }
+            },
+        
         {
             type: "input",
             name: "github",
-            message: "Enter your Github Username"
+            message: "Enter your Github Username (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+                }
         },
         {
             type: "input",
@@ -41,12 +58,28 @@ Add a New Project
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your project?'
+                message: 'What is the name of your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    }else {
+                        console.log('Please enter your project name!');
+                        return false;
+                    }
+                    }
             },
             {
                 type: 'input',
                 name: 'description',
-                message: "Provide a description of the project (Required)"
+                message: "Provide a description of the project (Required)",
+                validate: descriptionInput => {
+                    if (descriptionInput) {
+                        return true;
+                    }else {
+                        console.log('Please enter your project description!');
+                        return false;
+                    }
+                    }
             },
             {
                 type: 'checkbox',
@@ -57,7 +90,15 @@ Add a New Project
             {
                 type: 'confirm',
                 name: 'link',
-                message: "Enter the Github link to your project. (Required)"
+                message: "Enter the Github link to your project. (Required)",
+                validate: linkConfirm => {
+                    if (linkConfirm) {
+                        return true;
+                    }else {
+                        console.log('Please enter the project Github link!');
+                        return false;
+                    }
+                    }
             },
             {
                 type: 'confirm',
